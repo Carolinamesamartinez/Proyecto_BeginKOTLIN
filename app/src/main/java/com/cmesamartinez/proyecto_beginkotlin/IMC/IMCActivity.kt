ad9ac9d54680d17b115d4469c295ce192ac7aa4f
c.mesa.martinez <carolinamesamartinez1@gmail.com>
2023-04-11 17:42:57 +0200

    few errors 2

--- app/src/main/java/com/cmesamartinez/proyecto_beginkotlin/IMC/IMCActivity.kt
+++ app/src/main/java/com/cmesamartinez/proyecto_beginkotlin/IMC/IMCActivity.kt
@@ -110,7 +110,7 @@ class IMCActivity : AppCompatActivity() {
     }
 
     private fun calculateIMC():Double {
-        val df= DecimalFormat("#.##")
+        val df= DecimalFormat("#,##")
         val imc= currentWeight/(currentHeight.toDouble()/100*currentHeight.toDouble()/100)
         return  df.format(imc).toDouble()
     }
